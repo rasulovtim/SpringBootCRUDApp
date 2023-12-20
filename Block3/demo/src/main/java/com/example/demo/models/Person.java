3b8e5af246482d44cdfaa83c6ce213e0d8cc1ca5
rasulovtim <timmprogramming@gmail.com>
2023-12-20 17:26:35 +0500

    My commit 2 After correction by mentor 2

---
+++ Block3/demo/src/main/java/com/example/demo/models/Person.java
@@ -0,0 +1,75 @@
+package com.example.demo.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotEmpty
+    @Size(min = 2, max = 30, message = "Min 2 and max 30 characters")
+    @Column
+    private String username;
+    @Min(value = 1900, message = "Greater than 1900")
+    @Column(name = "year_of_birth")
+    private int yearOfBirth;
+    @Column(name = "password")
+    private String password;
+
+    public Person() {
+    }
+
+    public Person(String username, int yearOfBirth, String password) {
+        this.username = username;
+        this.yearOfBirth = yearOfBirth;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+               "id=" + id +
+               ", username='" + username + '\'' +
+               ", yearOfBirth=" + yearOfBirth +
+               ", password='" + password + '\'' +
+               '}';
+    }
+}
